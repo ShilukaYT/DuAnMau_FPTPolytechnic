cf19c6e21ed333deac39f6033b0120f52ba42ea3
namwd <namwd@DESKTOP-F6OO7UA>
2025-06-05 08:15:51 +0700

    Tắt ứng dụng form đăng nhập

--- LoginRegistrationForm/Form1.Designer.cs
+++ LoginRegistrationForm/Form1.Designer.cs
@@ -55,9 +55,8 @@ namespace LoginRegistrationForm
             this.panel1.Controls.Add(this.label2);
             this.panel1.Controls.Add(this.label1);
             this.panel1.Location = new System.Drawing.Point(-1, -1);
-            this.panel1.Margin = new System.Windows.Forms.Padding(4);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(450, 340);
+            this.panel1.Size = new System.Drawing.Size(338, 276);
             this.panel1.TabIndex = 0;
             this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
             // 
@@ -67,9 +66,10 @@ namespace LoginRegistrationForm
             this.label5.Cursor = System.Windows.Forms.Cursors.Hand;
             this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label5.ForeColor = System.Drawing.Color.Black;
-            this.label5.Location = new System.Drawing.Point(418, 4);
+            this.label5.Location = new System.Drawing.Point(314, 3);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(29, 32);
+            this.label5.Size = new System.Drawing.Size(24, 26);
             this.label5.TabIndex = 11;
             this.label5.Text = "x";
             // 
@@ -78,10 +78,9 @@ namespace LoginRegistrationForm
             this.login_close.AutoSize = true;
             this.login_close.Cursor = System.Windows.Forms.Cursors.Hand;
             this.login_close.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.login_close.Location = new System.Drawing.Point(812, 12);
-            this.login_close.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.login_close.Location = new System.Drawing.Point(609, 10);
             this.login_close.Name = "login_close";
-            this.login_close.Size = new System.Drawing.Size(24, 24);
+            this.login_close.Size = new System.Drawing.Size(18, 18);
             this.login_close.TabIndex = 10;
             this.login_close.Text = "X";
             this.login_close.Click += new System.EventHandler(this.login_close_Click);
@@ -92,10 +91,9 @@ namespace LoginRegistrationForm
             this.login_registerHere.Cursor = System.Windows.Forms.Cursors.Hand;
             this.login_registerHere.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.login_registerHere.ForeColor = System.Drawing.Color.DeepSkyBlue;
-            this.login_registerHere.Location = new System.Drawing.Point(169, 268);
-            this.login_registerHere.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.login_registerHere.Location = new System.Drawing.Point(127, 218);
             this.login_registerHere.Name = "login_registerHere";
-            this.login_registerHere.Size = new System.Drawing.Size(114, 18);
+            this.login_registerHere.Size = new System.Drawing.Size(96, 15);
             this.login_registerHere.TabIndex = 9;
             this.login_registerHere.Text = "Đăng Ký Ngay";
             this.login_registerHere.Click += new System.EventHandler(this.login_registerHere_Click);
@@ -108,10 +106,9 @@ namespace LoginRegistrationForm
             this.login_btn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.login_btn.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.login_btn.ForeColor = System.Drawing.Color.Black;
-            this.login_btn.Location = new System.Drawing.Point(160, 191);
-            this.login_btn.Margin = new System.Windows.Forms.Padding(4);
+            this.login_btn.Location = new System.Drawing.Point(120, 155);
             this.login_btn.Name = "login_btn";
-            this.login_btn.Size = new System.Drawing.Size(133, 43);
+            this.login_btn.Size = new System.Drawing.Size(100, 35);
             this.login_btn.TabIndex = 6;
             this.login_btn.Text = "ĐĂNG NHẬP";
             this.login_btn.UseVisualStyleBackColor = false;
@@ -120,12 +117,11 @@ namespace LoginRegistrationForm
             // login_password
             // 
             this.login_password.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.login_password.Location = new System.Drawing.Point(118, 121);
-            this.login_password.Margin = new System.Windows.Forms.Padding(4);
+            this.login_password.Location = new System.Drawing.Point(88, 98);
             this.login_password.Multiline = true;
             this.login_password.Name = "login_password";
             this.login_password.PasswordChar = '*';
-            this.login_password.Size = new System.Drawing.Size(310, 36);
+            this.login_password.Size = new System.Drawing.Size(234, 30);
             this.login_password.TabIndex = 5;
             this.login_password.TextChanged += new System.EventHandler(this.login_password_TextChanged);
             // 
@@ -133,31 +129,28 @@ namespace LoginRegistrationForm
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(19, 128);
-            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Location = new System.Drawing.Point(14, 104);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(103, 25);
+            this.label3.Size = new System.Drawing.Size(81, 20);
             this.label3.TabIndex = 4;
             this.label3.Text = "Mật Khẩu:";
             // 
             // login_username
             // 
             this.login_username.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.login_username.Location = new System.Drawing.Point(118, 59);
-            this.login_username.Margin = new System.Windows.Forms.Padding(4);
+            this.login_username.Location = new System.Drawing.Point(88, 48);
             this.login_username.Multiline = true;
             this.login_username.Name = "login_username";
-            this.login_username.Size = new System.Drawing.Size(310, 36);
+            this.login_username.Size = new System.Drawing.Size(234, 30);
             this.login_username.TabIndex = 3;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(14, 65);
-            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Location = new System.Drawing.Point(10, 53);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(105, 25);
+            this.label2.Size = new System.Drawing.Size(82, 20);
             this.label2.TabIndex = 2;
             this.label2.Text = "Tài khoản:";
             // 
@@ -165,24 +158,23 @@ namespace LoginRegistrationForm
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("MS UI Gothic", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(156, 10);
-            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Location = new System.Drawing.Point(117, 8);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(145, 24);
+            this.label1.Size = new System.Drawing.Size(118, 19);
             this.label1.TabIndex = 1;
             this.label1.Text = "ĐĂNG NHẬP";
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(450, 340);
+            this.ClientSize = new System.Drawing.Size(338, 276);
             this.Controls.Add(this.panel1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
-            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
             this.ResumeLayout(false);
